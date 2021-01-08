4453be4e31eb8a3cb36a83fac9b9868619a573e7
Marius Lindegård <marius.berg.lindegaard@gmail.com>
2021-01-08 12:42:56 +0000

    Added a cog loading/unloading system

---
+++ chat_commands.py
@@ -0,0 +1,18 @@
+#Here are all the basic chat commads
+from discord.ext import commands
+
+class ChatCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Command has been registered")
+
+    @commands.command()
+    async def test(self, ctx):
+      await ctx.send("test")
+
+def setup(bot):
+    bot.add_cog(ChatCommands(bot))
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #This is the experimental version of the bot!
 import discord, datetime, json, os, codecs
-from discord.ext import commands  #, tasks
+from discord.ext import commands, tasks
 from termcolor import colored
 from os import path
 
-#This need to stay on top of the code
+#This need to stay on top of the code after the imports
 print(colored('----STARTING DISCORD BOT----', 'green'))
 startup_time = datetime.datetime.now()
 print(startup_time.strftime("Time: %H:%M:%S"))
 print()
-#----------------------------------------
+#---------------------------------------
+
+extensions_array = ["chat_commands"]
 
 config = None
 prefix = "!"
@@ -28,12 +30,64 @@ def save_config():
     with codecs.open('config.json', 'w', encoding='utf8') as File:
         json.dump(config, File, sort_keys=True, indent=4, ensure_ascii=False)
 
+def space(spaces):
+  for x in range(spaces):
+    print()
+
 token = config["token"]
 
 intents = discord.Intents.default()
 bot = commands.Bot(
     command_prefix=prefix, case_insensitive=True, intents=intents)
 
+@bot.command()
+async def refresh(ctx):
+  for extension in extensions_array:
+    try:
+      bot.reload_extension(extension)
+    except:
+      bot.load_extension(extension)
+
+@bot.command()
+async def extensions(ctx):
+  await ctx.send("Extensions: \nchat_commands")
+  for extension in extensions_array:
+    await ctx.send(extension)
+
+@bot.command()
+async def unload_all_extensions(ctx):
+  for extension in extensions_array:
+    bot.unload_extension(extension)
+
+@bot.command()
+async def load_extension(ctx, extension):
+  if extension in extensions_array:
+    try:
+      bot.load_extension(extension)
+      await ctx.send("Extension has been successfully loaded")
+    except:
+      await ctx.send("This extension was already loaded")
+  else:
+    await ctx.send("This extension does not exist")
+
+@bot.command()
+async def unload_extension(ctx, extension):
+  if extension in extensions_array:
+    try:
+      bot.unload_extension(extension)
+      await ctx.send("Successfully unloaded extension")
+    except:
+      await ctx.send("Extension could either not be loaded, or something else went wrong")
+  else:
+    await ctx.send("This extension does not exist")
+
+@tasks.loop(hours=1)
+async def reload_extensions():
+  for extension in extensions_array:
+    try:
+      bot.reload_extension(extension)
+    except:
+      bot.load_extension(extension)
 
 @bot.event
 async def on_ready():
@@ -41,20 +95,15 @@ async def on_ready():
     bot_ready_time = datetime.datetime.now()
     print(bot_ready_time.strftime("Time: %H:%M:%S"))
     time = bot_ready_time - startup_time
-    print(colored("Start up time took:", "green"), colored(time, "red"))
-    print()
-    print()
+    print(colored("Start up time:", "green"), colored(time, "red"))
+    space(2)
     print('Logged in as {0.user}'.format(bot))
     print()
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="Jonas, I see what your doing"))
-
-@bot.command()
-async def ping(ctx):
-    print("Command has been registered")
-    await ctx.send("Din kommando has blitt registrert!")
+            name="Just a regular discord bot"))
+    reload_extensions.start()
 
 
 bot.run(token)
