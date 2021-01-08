5642ea7984e021b9f1f00a900a681b9685649166
Marius Lindegård <marius.berg.lindegaard@gmail.com>
2021-01-08 09:41:11 +0000

    Change from token.txt to config placeholder

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ This repo is for the Experimental version of my main bot MelonKami, here I will 
 ## Important Notes
 
 * If you want to use this bot, please create a token.txt file, and add your token there, nothing else
+
+## Important config.json Notes
+
+* CHANGE YOUR TOKEN! Change the token in the config to your actual token
+
+* If you don't want "!" to be the prefix, change it in the config
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 #This is not the original version!
-import discord, datetime
+import discord, datetime, json, os, codecs
 from discord.ext import commands  #, tasks
 from termcolor import colored
+from os import path
 
-with open ("token.txt") as token_code:
-  token = token_code.read()
-
+#This need to stay on top of the code
 print(colored('----STARTING DISCORD BOT----', 'green'))
 startup_time = datetime.datetime.now()
 print(startup_time.strftime("Time: %H:%M:%S"))
 print()
+#---------------------------------------
+
+config = None
+prefix = "!"
+
+if path.exists('config.json') == False:
+    with open('config.json', 'x') as File:
+        json.dump({}, File, indent=4)
+
+with codecs.open('config.json', 'r', encoding='utf-8-sig') as File:
+    config = json.load(File)
+
+def save_config():
+    with codecs.open('config.json', 'w', encoding='utf8') as File:
+        json.dump(config, File, sort_keys=True, indent=4, ensure_ascii=False)
+
+token = config["token"]
 
 intents = discord.Intents.default()
-
-bot = commands.Bot(command_prefix="!", case_insensitive=True, intents=intents)
+bot = commands.Bot(
+    command_prefix=prefix, case_insensitive=True, intents=intents)
 
 
 @bot.event
@@ -27,11 +43,16 @@ async def on_ready():
     print()
     print('Logged in as {0.user}'.format(bot))
     print()
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Jonas, I see what your doing"))
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name="Jonas, I see what your doing"))
+
 
 @bot.command()
 async def ping(ctx):
     print("Command has been registered")
     await ctx.send("Din kommando has blitt registrert!")
+
 
 bot.run(token)
